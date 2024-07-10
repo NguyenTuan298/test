93d930d978bd0955a509222785e78ed4fc68959b
Tuan <vanthi11bt@gmail,com>
2024-07-10 21:18:36 +0700

    first commit

---
+++ Test/Hello.java
@@ -0,0 +1,29 @@
+public class Hello {
+    public static void main(String[] args) {
+        int interger = 5;
+        int x = 5;
+        int y = 10;
+        String last_name = "Nguyen"; 
+        String fist_name = "Tuan";
+        System.out.println(interger);
+
+        System.out.println(fist_name);
+        
+        System.out.println(last_name + " " + fist_name);
+        System.out.print("x + y = ");
+        System.out.println(x + y);
+
+        System.out.println("hello");
+        
+        int myAge = 20;
+        int votinAge = 18;
+        if(myAge >= votinAge)
+        {
+            System.out.println("Old enough to vote!");
+        }
+        else
+        {
+            System.out.println("not Old enough to vote!");
+        }
+    }
+}
